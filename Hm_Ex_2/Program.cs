530d8c307eecf67b0c52c6d87af13d228463aaa1
Leon <turkov.leon-2@yandex.ru>
2022-08-28 01:33:58 +0300

    Added 1-st exersice

---
+++ Hm_Ex_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+
+int numA = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+if (numA > 999 || numA < 99)
+{
+    Console.WriteLine("Число не трёхзначное");
+}
+else
+{
+    numA = (int)numA / 10;
+    result = numA % 10;
+
+    Console.WriteLine("Второе число: " + result);
+}
