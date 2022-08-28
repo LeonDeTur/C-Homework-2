f4b03040230e7bf663cbff089c21935fa91441e5
Leon <turkov.leon-2@yandex.ru>
2022-08-28 11:11:43 +0300

    Added 13-th exersice

---
+++ Hm_Ex_13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число с тремя цифрами или больше");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+while (numA > 999)
+{
+    numA = numA/10;
+}
+
+if (numA < 100)
+{
+    Console.WriteLine("Введённое число двузначное или меньше");
+}
+else
+{
+    numA = numA % 10;
+    Console.WriteLine("Третья цифра: " + numA);
+}
