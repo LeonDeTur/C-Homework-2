19236c69deec53792dff75bc27e4458c93ebdcb2
Leon <turkov.leon-2@yandex.ru>
2022-08-29 01:15:23 +0300

    Added 15-th exersice

---
+++ Hm_Ex_15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите номер дня недели (от 1 до 7).");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if (numA < 1 || numA > 7)
+{
+    Console.WriteLine("Такого дня недели нет");
+} 
+else
+{
+    if (numA < 6)
+    {
+        Console.WriteLine("Введённый день недели не является выходным");
+    }
+    else
+    {
+        Console.WriteLine("Введённый день выходной");
+    }
+}
